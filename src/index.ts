728e016ddb6ed94793c45541f1ebefbee33c24ec
Matthew Mercuri <matthew@finnovate.io>
2026-02-11 11:02:08 -0500

    WIP Lendbae

--- src/index.ts
+++ src/index.ts
@@ -546,6 +546,77 @@ async function runCrunchyWithLocalCsv(inputRelativePath: string, segment: RaiseS
   writeToCsv(`PROCESSED_${timestamp}_${inputRelativePath}`, validatedFinalRows)
 }
 
+// const LendBaeInputSchema = z.object({
+//   'Company Name': z.string(),
+//   'Website': z.string(),
+//   'Company City': z.string(),
+//   'SIC Codes': z.string(),
+//   'NAICS Codes': z.string(),
+//   'Short Description': z.string()
+// })
+// type LendBaeInput = z.infer<typeof LendBaeInputSchema>
+
+// async function runLendbaeWithLocalCsv(inputRelativePath: string) {
+//   const { rows, totalRows } = await getInputFromCsv(inputRelativePath)
+
+//   const cleanedRows: LendBaeInput[] = []
+//   rows.forEach((row) => {
+//     const { success, data } = z.safeParse(LendBaeInputSchema, row)
+//     if (success) {
+//       cleanedRows.push(data)
+//     }
+//   })
+
+//   const context: PipelineContext = {
+//     logger: new RunLogger(),
+//     tracker: new RunTracker(),
+//     throwError: (message: string) => {
+//       throw new Error(message)
+//     },
+//     config: {
+//       titlesToSearch: crunchyConfig.bestTitle.titlePriorities[segment],
+//       options: crunchyConfig.options[segment],
+//       totalRows
+//     }
+//   }
+
+//   const pipeline = Pipeline.create<Input>()
+//     .pipe(new PreProcessStage())
+//     .pipe(new GetOrganizationStage())
+//     .pipe(new GetPeopleStage())
+//     .pipe(new GetBestContactStage())
+//     .pipe(new EnrichContactStage())
+//     .pipe(new PostProcessStage())
+
+//   const completedRows: Output[] = []
+//   for (const row of cleanedRows) {
+//     try {
+//       const result = await pipeline.run(row, context)
+//       completedRows.push(result)
+//     } catch (err) {
+//       if (err instanceof Error) {
+//         context.logger.error(`PIPELINE ERROR - ${err.message}`)
+//       } else {
+//         context.logger.error('Unknown Pipeline Error')
+//       }
+//     }
+//   }
+
+//   const validatedFinalRows: Output[] = []
+//   completedRows.forEach(completedRow => {
+//     const { success, data } = z.safeParse(OutputSchema, completedRow)
+//     if (success) {
+//       validatedFinalRows.push(data)
+//     }
+//   })
+
+//   context.tracker.logSummaryStats()
+
+//   const timestamp = new Date().toISOString();
+//   writeToCsv(`PROCESSED_${timestamp}_${inputRelativePath}`, validatedFinalRows)
+// }
+
+
 async function main() {
   runCrunchyWithLocalCsv('Crunchy2026JanuaryBCCS.csv', 'BCCS')
 }
